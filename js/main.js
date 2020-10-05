21a8947b0e212b7987eb933948f2aaa9192be4b2
folimartin <folimrtn@gmail.com>
2020-10-05 02:59:44 -0700

    initial commit

--- js/main.js
+++ js/main.js
@@ -4,13 +4,5 @@ AOS.init({
     easing: 'ease-out-back',
     duration: 100
 });
-// hero sliding blinking problem solved
-var images = new Array()
 
-function preload() {
-    for (i = 0; i < preload.arguments.length; i++) {
-        images[i] = new Image()
-        images[i].src = preload.arguments[i]
-    }
-}
-preload("../img/profile/me.jpg", "../img/profile/me1.jpg", "../img/profile/me3.jpg")
+
