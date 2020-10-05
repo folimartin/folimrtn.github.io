77aa058e08ea371ad2172529f9ebf848d5f08c32
folimartin <folimrtn@gmail.com>
2020-10-05 03:43:01 -0700

    initial commit

--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@ AOS.init({
     duration: 100
 });
 
+// auto typing text
+var typed = new Typed('.typing-text', {
+    strings: ["Foli Martin"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+});
 
